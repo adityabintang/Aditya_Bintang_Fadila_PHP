2d0400a043c57690bf420ec45198d915838d9bee
Aditya Bintang F <45384894+adityabintang@users.noreply.github.com>
2021-05-04 09:08:12 +0700

    menambahkan jawaban pada soal no.5.php

--- aditya_bintang_php_5.php
+++ aditya_bintang_php_5.php
@@ -1,12 +1,10 @@
 <?php
-    
-$a = 11;
-if ($a > 1) {
-    echo '1.Bilangan Ganjil';
-}
-
-
-
-
-
+for ($i=0; $i < 1; $i++) { 
+        if (true) {
+           echo "makanan<br> 1. Menu Makanan ke 1<br> 2. Menu Makanan ke 2<br> 3. Menu Makanan ke 3<br> Minuman<br> 1. Menu Minuman ke 1<br> 2. Menu Minuman ke 2<br> 3. Menu Minuman ke 3<br> Parsel<br> 1. Menu Parsel ke 1<br> 2. Menu Parsel ke 2<br>3. Menu Parsel ke 3<br>";
+        }else {
+             echo "Hallo";
+        } 
+    }
+   
 ?>
